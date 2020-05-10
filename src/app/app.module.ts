61ec79254187a80b64c5181277ccd2df6e315cc7
veeresh09 <veeresh1729@gmail.com>
2020-05-10 23:13:16 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.componenet.html',
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
   enteredValue = ' ';
